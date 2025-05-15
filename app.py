9c85672f0c3b270fd02fa94b0b84b65f08e5ec3f
anamyaaa <94094603+anamyaaa@users.noreply.github.com>
2025-05-16 03:30:38 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
@@ -5,6 +6,50 @@ import pandas as pd
 app = Flask(__name__)
 
 model = joblib.load('nids_pipeline.pkl')  # Make sure this file is in the same folder
+
+sample_input = [{
+    "duration": 0,
+    "protocol_type": "tcp",
+    "service": "http",
+    "flag": "SF",
+    "src_bytes": 181,
+    "dst_bytes": 5450,
+    "land": 0,
+    "wrong_fragment": 0,
+    "urgent": 0,
+    "hot": 0,
+    "num_failed_logins": 0,
+    "logged_in": 1,
+    "num_compromised": 0,
+    "root_shell": 0,
+    "su_attempted": 0,
+    "num_root": 0,
+    "num_file_creations": 0,
+    "num_shells": 0,
+    "num_access_files": 0,
+    "num_outbound_cmds": 0,
+    "is_host_login": 0,
+    "is_guest_login": 0,
+    "count": 511,
+    "srv_count": 511,
+    "serror_rate": 0.0,
+    "srv_serror_rate": 0.0,
+    "rerror_rate": 0.0,
+    "srv_rerror_rate": 0.0,
+    "same_srv_rate": 1.0,
+    "diff_srv_rate": 0.0,
+    "srv_diff_host_rate": 0.0,
+    "dst_host_count": 255,
+    "dst_host_srv_count": 255,
+    "dst_host_same_srv_rate": 1.0,
+    "dst_host_diff_srv_rate": 0.0,
+    "dst_host_same_src_port_rate": 0.03,
+    "dst_host_srv_diff_host_rate": 0.0,
+    "dst_host_serror_rate": 0.0,
+    "dst_host_srv_serror_rate": 0.0,
+    "dst_host_rerror_rate": 0.0,
+    "dst_host_srv_rerror_rate": 0.0
+}]
 
 @app.route('/')
 def home():
@@ -14,6 +59,12 @@ def home():
 def predict():
     data = request.get_json(force=True)
     df = pd.DataFrame(data)
+    prediction = model.predict(df)
+    return jsonify({'prediction': prediction.tolist()})
+
+@app.route('/test', methods=['GET'])
+def test():
+    df = pd.DataFrame(sample_input)
     prediction = model.predict(df)
     return jsonify({'prediction': prediction.tolist()})
 
